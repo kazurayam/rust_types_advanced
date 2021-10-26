67c6cf888151ceeda71bdfcb8fa16f46d6638274
kazurayam <kazuaki.urayama@gmail.com>
2021-10-26 22:52:50 +0900

    Listing19-27

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-type Kilometers = i32;
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
 
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
 fn main() {
-    let x: i32 = 5;
-    let y: Kilometers = 5;
-    println!("x + y = {}", x + y);
+    let answer = do_twice(add_one, 5);
+    println!("{}", answer);
 }
